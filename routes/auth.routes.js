b7312c2a7983812aebd419edeac74ba7c17672bc
Jisan <jishananam@iut-dhaka.edu>
2021-06-28 00:49:03 +0600

    Can Successfully Fetch Data

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,20 @@
 
 const express=require('express')
 const routes=express.Router()
+const bodyParser=require('body-parser')
+routes.use(bodyParser.urlencoded({ extended: false }));
+
 const getLogin=require('../controllers/auth.controller')
+
 /*const getLogin=(req,res)=>{
     res.sendFile('login.html',{root:'./views'})
 
 }*/
 routes.get('/',getLogin)
+routes.post('/',(req,res)=>{
+    const email=req.body.email
+    const password=req.body.password
+    console.log(`Email ${email}  Password ${password}`)
+})
 
 module.exports=routes
