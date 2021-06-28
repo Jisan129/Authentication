01aa2bd99fa87d85f8b4c4435bbe4d52e914ec73
Jisan <jishananam@iut-dhaka.edu>
2021-06-28 15:57:55 +0600

    Bcrypt :)

--- routes/register.js
+++ routes/register.js
@@ -4,14 +4,22 @@ const bodyParser=require('body-parser')
 routes.use(bodyParser.urlencoded({ extended: false }));
 const getRegistration=require('../controllers/registration.controller')
 const connection = require('../connection')
-
+const bcrypt=require('bcrypt');
+const saltRound=10;
 
 routes.get('/',getRegistration)
 
 routes.post('/',(req, res)=>{
     const email = req.body.email
     const password = req.body.password
-    const queryString =  `INSERT INTO user 
+
+    var newpassword=''
+    bcrypt.genSalt(10).then(salt=>{
+        console.log(`Salt ${salt}`);
+        return bcrypt.hash(password,salt)
+    }).then(hash=>{
+        newpassword=hash
+        const queryString =  `INSERT INTO user 
             (
                 email,password
             )
@@ -20,15 +28,20 @@ routes.post('/',(req, res)=>{
                 ?, ?
             )`
 
-    connection.query(queryString,[email,password], (err, rows, fields) => {
-        if (!err) {
-            res.send(rows)
-        } else {
-            console.log(err)
-        }
+        connection.query(queryString,[email,newpassword], (err, rows, fields) => {
+            if (!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+        })
     })
+        .catch(err=>console.error(err.message))
 
-    console.log(`Email ${email}  Password ${password}`)
+
+
+
+
 
 })
 
